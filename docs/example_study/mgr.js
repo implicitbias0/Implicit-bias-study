302af109d3c3eedd5b0fc3c2a06487eea6e95068
implicitbias0 <163656527+implicitbias0@users.noreply.github.com>
2024-03-16 17:52:41 +0100

    Update mgr.js

--- docs/example_study/mgr.js
+++ docs/example_study/mgr.js
@@ -9,7 +9,7 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
+	init_data_pipe(API, 'hDEA2F0X9fwPvUWpcjRO1Dp0G9efiGFxNKCpZcP8ypUKV5lM5vbYH7IqctQ6iEoJe5LHSb',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
